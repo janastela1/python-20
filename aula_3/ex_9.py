4550484be8c16a95724c8ba0617b2c44aa85f201
Ivan Ferro <iaferro@gmail.com>
2020-03-03 21:24:56 -0300

    atualizacao dos arquivos

--- aula_3/ex_9.py
+++ aula_3/ex_9.py
@@ -1,6 +1,23 @@
 
 # encontrar somente os usuarios que tem o cpf informado
 
+usuarios  = [
+
+    {
+        'id' : 1 ,
+        'email' : 'user_1@email.com' ,
+        'pagou_conta' : falso
+    }
+
+]
+
+def  filtro ( usuario ):
+    se  usuario [ 'pagou_conta' ] ==  True :
+        retornar  falso
+    return  True
+
+def  filtar_usuarios ( lista ):
+     lista de retorno ( filtro ( filtro , lista ))
 
 
 
